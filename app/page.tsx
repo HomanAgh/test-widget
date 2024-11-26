4ba754a144e9c4fff072149ccc21f91e53e69d44
HomanAgh <145568266+HomanAgh@users.noreply.github.com>
2024-11-26 01:40:38 +0100

    reorginazed folders, renamed files, updates components so it goes from /auth to /player

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,41 @@
-'use client';
+/* "use client";
 
-import React from 'react';
-import PlayerPage from './pages/player';
-import LoginPage from './pages/auth';
+import React from "react";
+import PlayerPage from "./player";
+import LoginPage from "./auth/page";
 
 const App = () => {
-  const isLoggedIn = typeof window !== 'undefined' && localStorage.getItem('isLoggedIn') === 'true';
+  const isLoggedIn =
+    typeof window !== "undefined" &&
+    localStorage.getItem("isLoggedIn") === "true";
 
   return isLoggedIn ? <PlayerPage /> : <LoginPage />;
 };
 
-export default App;
+export default App; */
+
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+const HomePage = () => {
+  const router = useRouter();
+  const isLoggedIn =
+    typeof window !== "undefined" &&
+    localStorage.getItem("isLoggedIn") === "true";
+
+  useEffect(() => {
+    if (!isLoggedIn) {
+      router.push("/auth"); // Redirect to the login page
+    }
+  }, [isLoggedIn, router]);
+
+  return (
+    <div>
+    </div>
+  );
+};
+
+export default HomePage;
+
