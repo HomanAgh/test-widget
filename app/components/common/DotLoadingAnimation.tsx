53e2bda9e1f92f7e6d13bc75074e4374000a4832
HomanAgh <145568266+HomanAgh@users.noreply.github.com>
2025-02-13 15:55:53 +0100

    added loading spinner and small fix for loading for alumni page

--- app/components/common/DotLoadingAnimation.tsx
+++ app/components/common/DotLoadingAnimation.tsx
@@ -5,11 +5,12 @@ const DotLoadingAnimation: React.FC = () => {
     <div className="flex flex-col items-center space-y-2">
     
         <div className="flex space-x-1">
-            <span className="w-2 h-2 bg-blue-950 rounded-full animate-bounce" style={{ animationDelay: "0s" }}></span>
-            <span className="w-2 h-2 bg-blue-950 rounded-full animate-bounce" style={{ animationDelay: "0.2s" }}></span>
-            <span className="w-2 h-2 bg-blue-950 rounded-full animate-bounce" style={{ animationDelay: "0.4s" }}></span>
-            <span className="w-2 h-2 bg-blue-950 rounded-full animate-bounce" style={{ animationDelay: "0.6s" }}></span>
-            <span className="w-2 h-2 bg-blue-950 rounded-full animate-bounce" style={{ animationDelay: "0.8s" }}></span>
+            <span className="w-6 h-6 bg-[#052D41] rounded-full animate-bounce" style={{ animationDelay: "0s" }}></span>
+            <span className="w-6 h-6 bg-[#052D41] rounded-full animate-bounce" style={{ animationDelay: "0.2s" }}></span>
+            <span className="w-6 h-6 bg-[#052D41] rounded-full animate-bounce" style={{ animationDelay: "0.4s" }}></span>
+            <span className="w-6 h-6 bg-[#052D41] rounded-full animate-bounce" style={{ animationDelay: "0.6s" }}></span>
+            <span className="w-6 h-6 bg-[#052D41] rounded-full animate-bounce" style={{ animationDelay: "0.8s" }}></span>
+            <span className="w-6 h-6 bg-[#052D41] rounded-full animate-bounce" style={{ animationDelay: "1s" }}></span>
         </div>
     </div>
 
