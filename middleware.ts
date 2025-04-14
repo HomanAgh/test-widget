910a606f80fc0fb111e70739364759240cebde8d
AlbinKL1 <proguy1998@outlook.com>
2025-04-14 11:58:26 +0200

    middleware crash fix.

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,10 @@
+import { type NextRequest } from 'next/server'
+import { updateSession } from './app/utils/supabase/middleware'
 
+export async function middleware(request: NextRequest) {
 
-// export async function middleware(request: NextRequest) {
-//   // Skip middleware for widget bundle files
-//   if (
-//     request.nextUrl.pathname.startsWith('/widget-bundle.js') || 
-//     request.nextUrl.pathname.startsWith('/widget-loader.js') || 
-//     request.nextUrl.pathname.startsWith('/widget-loader-combined.js') ||
-//     request.nextUrl.pathname.endsWith('.html')||
-//     request.nextUrl.pathname.startsWith('/_next/image') && request.nextUrl.searchParams.has('url')
-//   ) {
-//     return;
-//   }
-  
-//   return await updateSession(request)
-// }
+  return
+}
 
 export const config = {
   matcher: [
